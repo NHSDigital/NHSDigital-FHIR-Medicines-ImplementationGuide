8d3cbeff8d5d5756c1ac7124480e4feea17ef2fd
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2020-06-17 06:56:41 +0100

    Package name shortened

--- Examples/profileValidationTests.bat
+++ Examples/profileValidationTests.bat
@@ -1,35 +1,35 @@
 rem PASS - [Bundle] Prescription Order Message
 
-java -jar org.hl7.fhir.validator.jar Bundle-prescription-order.xml -version 4.0.1 -ig https://packages.simplifier.net/uk.ElectronicPrescriptionService.dev/-/uk.ElectronicPrescriptionService.dev-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ 
+java -jar org.hl7.fhir.validator.jar Bundle-prescription-order.xml -version 4.0.1 -ig https://packages.simplifier.net/UK.MM.r4/-/UK.MM.r4-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ 
 
 rem PASS - [MedicationRequest] Multiple medication codes validation
 
-java -jar org.hl7.fhir.validator.jar MedicationRequest-multipleMedicationCodes-pass.xml -version 4.0.1 -ig https://packages.simplifier.net/uk.ElectronicPrescriptionService.dev/-/uk.ElectronicPrescriptionService.dev-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-MedicationRequest
+java -jar org.hl7.fhir.validator.jar MedicationRequest-multipleMedicationCodes-pass.xml -version 4.0.1 -ig https://packages.simplifier.net/UK.MM.r4/-/UK.MM.r4-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-MedicationRequest
 
 rem FAIL - [MedicationRequest] Missing SNOMED medication codes
 
-java -jar org.hl7.fhir.validator.jar MedicationRequest-missingSNOMEDMedicationCodes-fail.xml -version 4.0.1 -ig https://packages.simplifier.net/uk.ElectronicPrescriptionService.dev/-/uk.ElectronicPrescriptionService.dev-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-MedicationRequest
+java -jar org.hl7.fhir.validator.jar MedicationRequest-missingSNOMEDMedicationCodes-fail.xml -version 4.0.1 -ig https://packages.simplifier.net/UK.MM.r4/-/UK.MM.r4-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-MedicationRequest
 
 rem PASS - [MedicationRequest] non eps profile specified, must validate against MM profile pass (note will fail if resource doesn't also validate against specified profile)
 
-java -jar org.hl7.fhir.validator.jar MedicationRequest-alienProfile-pass.xml -version 4.0.1 -ig https://packages.simplifier.net/uk.ElectronicPrescriptionService.dev/-/uk.ElectronicPrescriptionService.dev-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-MedicationRequest
+java -jar org.hl7.fhir.validator.jar MedicationRequest-alienProfile-pass.xml -version 4.0.1 -ig https://packages.simplifier.net/UK.MM.r4/-/UK.MM.r4-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-MedicationRequest
 
 rem PASS - [PractitionerRole] 
 
-java -jar org.hl7.fhir.validator.jar PractitionerRole-pass.xml -version 4.0.1 -ig https://packages.simplifier.net/uk.ElectronicPrescriptionService.dev/-/uk.ElectronicPrescriptionService.dev-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-PractitionerRole
+java -jar org.hl7.fhir.validator.jar PractitionerRole-pass.xml -version 4.0.1 -ig https://packages.simplifier.net/UK.MM.r4/-/UK.MM.r4-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-PractitionerRole
 
 rem FAIL - [Patient] PDS
 
-java -jar org.hl7.fhir.validator.jar Patient-PDS-pass.json -version 4.0.1 -ig https://packages.simplifier.net/uk.ElectronicPrescriptionService.dev/-/uk.ElectronicPrescriptionService.dev-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-Patient
+java -jar org.hl7.fhir.validator.jar Patient-PDS-pass.json -version 4.0.1 -ig https://packages.simplifier.net/UK.MM.r4/-/UK.MM.r4-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-Patient
 
 rem FAIL - [Patient] EPS
 
-java -jar org.hl7.fhir.validator.jar Patient-EPS-pass.json -version 4.0.1 -ig https://packages.simplifier.net/uk.ElectronicPrescriptionService.dev/-/uk.ElectronicPrescriptionService.dev-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-Patient
+java -jar org.hl7.fhir.validator.jar Patient-EPS-pass.json -version 4.0.1 -ig https://packages.simplifier.net/UK.MM.r4/-/UK.MM.r4-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-Patient
 
 rem PASS - [Practitioner] 
 
-java -jar org.hl7.fhir.validator.jar Practitioner.xml -version 4.0.1 -ig https://packages.simplifier.net/uk.ElectronicPrescriptionService.dev/-/uk.ElectronicPrescriptionService.dev-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-Practitioner
+java -jar org.hl7.fhir.validator.jar Practitioner.xml -version 4.0.1 -ig https://packages.simplifier.net/UK.MM.r4/-/UK.MM.r4-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-Practitioner
 
 rem PASS - [Organization] 
 
-java -jar org.hl7.fhir.validator.jar Organization.xml -version 4.0.1 -ig https://packages.simplifier.net/uk.ElectronicPrescriptionService.dev/-/uk.ElectronicPrescriptionService.dev-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-Organization
+java -jar org.hl7.fhir.validator.jar Organization.xml -version 4.0.1 -ig https://packages.simplifier.net/UK.MM.r4/-/UK.MM.r4-0.0.3-dev.tgz -ig C:\Development\NHSD\eps\StructureDefinition\ -profile https://fhir.nhs.uk/R4/StructureDefinition/MM-Organization
