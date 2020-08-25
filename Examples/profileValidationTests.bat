8c04920e6baf8cbca18a901efd324df5183cad34
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2020-08-25 11:55:33 +0100

    Fault with dodgy extension added to MedicationRequest profile

--- Examples/profileValidationTests.bat
+++ Examples/profileValidationTests.bat
@@ -1,83 +1,89 @@
 rem PASS - [Bundle] Prescription Order Message
 
-java -jar org.hl7.fhir.validator.jar Bundle-prescription-order.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz   
+java -jar org.hl7.fhir.validator.jar Bundle-prescription-order.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz   
 
 
-rem PASS - [Bundle] Outpatient Prescription Order 
+rem PASS - [Bundle] Outpatient Prescription Order 4
 
-java -jar org.hl7.fhir.validator.jar outpatient-4-example.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz   
+java -jar org.hl7.fhir.validator.jar outpatient-4-example.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz   
+
+
+rem PASS - [Bundle] Outpatient Prescription Order 1
+
+java -jar org.hl7.fhir.validator.jar outpatient-1-example.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz   
+
 
 rem PASS - [Bundle] Homecare Prescription Order 
 
-java -jar org.hl7.fhir.validator.jar Homecare-example.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz   
+java -jar org.hl7.fhir.validator.jar Homecare-example.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz   
 
 
 
 rem PASS [Bundle] Solutions Assurance Order Message
 
-java -jar org.hl7.fhir.validator.jar 3C2366-B81001-0A409U.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz   
+java -jar org.hl7.fhir.validator.jar 3C2366-B81001-0A409U.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz   
 
 
 rem PASS - [MedicationRequest] Multiple medication codes validation
 
-java -jar org.hl7.fhir.validator.jar MedicationRequest-multipleMedicationCodes-pass.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-MedicationRequest
+java -jar org.hl7.fhir.validator.jar MedicationRequest-multipleMedicationCodes-pass.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-MedicationRequest
 
 rem FAIL - [MedicationRequest] Missing SNOMED medication codes
 
-java -jar org.hl7.fhir.validator.jar MedicationRequest-missingSNOMEDMedicationCodes-fail.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-MedicationRequest
+java -jar org.hl7.fhir.validator.jar MedicationRequest-missingSNOMEDMedicationCodes-fail.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-MedicationRequest
 
 rem PASS - [MedicationRequest] non eps profile specified, must validate against MM profile pass (note will fail if resource doesn't also validate against specified profile)
 
-java -jar org.hl7.fhir.validator.jar MedicationRequest-alienProfile-pass.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-MedicationRequest
+java -jar org.hl7.fhir.validator.jar MedicationRequest-alienProfile-pass.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-MedicationRequest
 
 rem PASS - [MedicationRequest] repeat Dispensing
 
-java -jar org.hl7.fhir.validator.jar MedicationRequest-repeatDispensing-pass.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-MedicationRequest
+java -jar org.hl7.fhir.validator.jar MedicationRequest-repeatDispensing-pass.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-MedicationRequest
 
 rem [MedicationRequest] Better
 
-java -jar org.hl7.fhir.validator.jar MedicationRequest-better.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-MedicationRequest
+java -jar org.hl7.fhir.validator.jar MedicationRequest-better.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-MedicationRequest
 
 
 rem PASS - [PractitionerRole] 
 
-java -jar org.hl7.fhir.validator.jar PractitionerRole-pass.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/Spine-PractitionerRole
+java -jar org.hl7.fhir.validator.jar PractitionerRole-pass.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/Spine-PractitionerRole
 
 
 
 rem FAIL - [Patient] 
 
-java -jar org.hl7.fhir.validator.jar Patient-PDS-fail.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-Patient
+java -jar org.hl7.fhir.validator.jar Patient-PDS-fail.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-Patient
 
 rem PASS - [Patient] 
 
-java -jar org.hl7.fhir.validator.jar Patient-EPS-pass.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-Patient
+java -jar org.hl7.fhir.validator.jar Patient-EPS-pass.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-Patient
 
 
 
 rem PASS - [Practitioner] 
 
-java -jar org.hl7.fhir.validator.jar Practitioner.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/Spine-Practitioner
+java -jar org.hl7.fhir.validator.jar Practitioner.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/Spine-Practitioner
 
 
 
 rem PASS - [Organization] 
 
-java -jar org.hl7.fhir.validator.jar Organization.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/Spine-Organization
+java -jar org.hl7.fhir.validator.jar Organization.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/Spine-Organization
 
 rem PASS - [Organisation-2] 
 
-java -jar org.hl7.fhir.validator.jar Organisation-2.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/Spine-Organization
+java -jar org.hl7.fhir.validator.jar Organisation-2.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/Spine-Organization
 
 
 rem PASS - [PractitionerRole - json] 
 
-java -jar org.hl7.fhir.validator.jar PractitionerRole-2.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/Spine-PractitionerRole
+java -jar org.hl7.fhir.validator.jar PractitionerRole-2.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/Spine-PractitionerRole
 
 rem PASS - [CommunicationRequest - json] 
 
-java -jar org.hl7.fhir.validator.jar CommunicationRequest.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-CommunicationRequest
+java -jar org.hl7.fhir.validator.jar CommunicationRequest.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/DM-CommunicationRequest
 
 rem PASS - [List Repeat Medications] 
 
-java -jar org.hl7.fhir.validator.jar List-RepeatMedications.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.30-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/UKCore-List
+java -jar org.hl7.fhir.validator.jar List-RepeatMedications.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.33-dev.tgz  -profile https://fhir.nhs.uk/R4/StructureDefinition/UKCore-List
