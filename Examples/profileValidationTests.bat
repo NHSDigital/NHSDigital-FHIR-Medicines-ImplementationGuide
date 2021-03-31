f8ec9194f93898f75a9c9f20dd827b408ce2aed0
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2021-03-31 08:42:24 +0100

    Moved from Spine to NHSDigital IG Package

--- Examples/profileValidationTests.bat
+++ Examples/profileValidationTests.bat
@@ -52,38 +52,38 @@ java -jar org.hl7.fhir.validator.jar MedicationRequest-better.xml -version 4.0.1
 
 rem PASS - [PractitionerRole] 
 
-java -jar org.hl7.fhir.validator.jar PractitionerRole-pass.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.1.5-alpha.tgz  -profile https://fhir.nhs.uk/StructureDefinition/Spine-PractitionerRole
+java -jar org.hl7.fhir.validator.jar PractitionerRole-pass.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.1.5-alpha.tgz  -profile https://fhir.nhs.uk/StructureDefinition/NHSDigital-PractitionerRole
 
 
 
 rem FAIL - [Patient] 
 
-java -jar org.hl7.fhir.validator.jar Patient-PDS-fail.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.1.5-alpha.tgz  -profile https://fhir.nhs.uk/StructureDefinition/Spine-Patient
+java -jar org.hl7.fhir.validator.jar Patient-PDS-fail.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.1.5-alpha.tgz  -profile https://fhir.nhs.uk/StructureDefinition/NHSDigital-Patient
 
 rem PASS - [Patient] 
 
-java -jar org.hl7.fhir.validator.jar Patient-EPS-pass.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.1.5-alpha.tgz  -profile https://fhir.nhs.uk/StructureDefinition/Spine-Patient
+java -jar org.hl7.fhir.validator.jar Patient-EPS-pass.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.1.5-alpha.tgz  -profile https://fhir.nhs.uk/StructureDefinition/NHSDigital-Patient
 
 
 
 rem PASS - [Practitioner] 
 
-java -jar org.hl7.fhir.validator.jar Practitioner.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.1.5-alpha.tgz  -profile https://fhir.nhs.uk/StructureDefinition/Spine-Practitioner
+java -jar org.hl7.fhir.validator.jar Practitioner.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.1.5-alpha.tgz  -profile https://fhir.nhs.uk/StructureDefinition/NHSDigital-Practitioner
 
 
 
 rem PASS - [Organization] 
 
-java -jar org.hl7.fhir.validator.jar Organization.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.1.5-alpha.tgz  -profile https://fhir.nhs.uk/StructureDefinition/Spine-Organization
+java -jar org.hl7.fhir.validator.jar Organization.xml -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.1.5-alpha.tgz  -profile https://fhir.nhs.uk/StructureDefinition/NHSDigital-Organization
 
 rem PASS - [Organisation-2] 
 
-java -jar org.hl7.fhir.validator.jar Organisation-2.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.1.5-alpha.tgz  -profile https://fhir.nhs.uk/StructureDefinition/Spine-Organization
+java -jar org.hl7.fhir.validator.jar Organisation-2.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.1.5-alpha.tgz  -profile https://fhir.nhs.uk/StructureDefinition/NHSDigital-Organization
 
 
 rem PASS - [PractitionerRole - json] 
 
-java -jar org.hl7.fhir.validator.jar PractitionerRole-2.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.1.5-alpha.tgz  -profile https://fhir.nhs.uk/StructureDefinition/Spine-PractitionerRole
+java -jar org.hl7.fhir.validator.jar PractitionerRole-2.json -version 4.0.1 -tx https://r4.ontoserver.csiro.au/fhir -ig https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.1.5-alpha.tgz  -profile https://fhir.nhs.uk/StructureDefinition/NHSDigital-PractitionerRole
 
 rem PASS - [CommunicationRequest - json] 
 
