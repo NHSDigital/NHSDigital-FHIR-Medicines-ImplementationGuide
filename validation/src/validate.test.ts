512915218ef89cf7f98ad40a25878561ca75a2f2
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2021-10-28 15:59:14 +0100

    Added more resources to increase volume of testing

--- validation/src/validate.test.ts
+++ validation/src/validate.test.ts
@@ -32,6 +32,184 @@ describe('Parsing folder Examples', () => {
 });
 
 
+describe('Parsing folder CapabilityStatement', () => {
+    const dir = '../CapabilityStatement';
+    const list = fs.readdirSync(dir);
+    list.forEach(function (file) {
+        file = dir + "/" + file;
+        const resource: any = fs.readFileSync(dir + "/" + file, 'utf8');
+
+        it('Validate ' + file, async () => {
+            await client()
+                .post('/$validate')
+                .set("Content-Type", getContentType(file))
+                .set("Accept", 'application/fhir+json')
+                .send(resource)
+                .expect(200)
+                .then((response: any) => {
+                    resourceChecks(response, file)
+                })
+        });
+
+    })
+});
+
+
+describe('Parsing folder CodeSystem', () => {
+    const dir = '../CodeSystem';
+    const list = fs.readdirSync(dir);
+    list.forEach(function (file) {
+        file = dir + "/" + file;
+        const resource: any = fs.readFileSync(dir + "/" + file, 'utf8');
+
+        it('Validate ' + file, async () => {
+            await client()
+                .post('/$validate')
+                .set("Content-Type", getContentType(file))
+                .set("Accept", 'application/fhir+json')
+                .send(resource)
+                .expect(200)
+                .then((response: any) => {
+                    resourceChecks(response, file)
+                })
+        });
+
+    })
+});
+
+describe('Parsing folder MessageDefinition', () => {
+    const dir = '../MessageDefinition';
+    const list = fs.readdirSync(dir);
+    list.forEach(function (file) {
+        file = dir + "/" + file;
+        const resource: any = fs.readFileSync(dir + "/" + file, 'utf8');
+
+        it('Validate ' + file, async () => {
+            await client()
+                .post('/$validate')
+                .set("Content-Type", getContentType(file))
+                .set("Accept", 'application/fhir+json')
+                .send(resource)
+                .expect(200)
+                .then((response: any) => {
+                    resourceChecks(response, file)
+                })
+        });
+
+    })
+});
+
+describe('Parsing folder ValueSet', () => {
+    const dir = '../ValueSet';
+    const list = fs.readdirSync(dir);
+    list.forEach(function (file) {
+        file = dir + "/" + file;
+        const resource: any = fs.readFileSync(dir + "/" + file, 'utf8');
+
+        it('Validate ' + file, async () => {
+            await client()
+                .post('/$validate')
+                .set("Content-Type", getContentType(file))
+                .set("Accept", 'application/fhir+json')
+                .send(resource)
+                .expect(200)
+                .then((response: any) => {
+                    resourceChecks(response, file)
+                })
+        });
+
+    })
+});
+
+describe('Parsing folder ConceptMap', () => {
+    const dir = '../ConceptMap';
+    const list = fs.readdirSync(dir);
+    list.forEach(function (file) {
+        file = dir + "/" + file;
+        const resource: any = fs.readFileSync(dir + "/" + file, 'utf8');
+
+        it('Validate ' + file, async () => {
+            await client()
+                .post('/$validate')
+                .set("Content-Type", getContentType(file))
+                .set("Accept", 'application/fhir+json')
+                .send(resource)
+                .expect(200)
+                .then((response: any) => {
+                    resourceChecks(response, file)
+                })
+        });
+
+    })
+});
+
+describe('Parsing folder SearchParameter', () => {
+    const dir = '../SearchParameter';
+    const list = fs.readdirSync(dir);
+    list.forEach(function (file) {
+        file = dir + "/" + file;
+        const resource: any = fs.readFileSync(dir + "/" + file, 'utf8');
+
+        it('Validate ' + file, async () => {
+            await client()
+                .post('/$validate')
+                .set("Content-Type", getContentType(file))
+                .set("Accept", 'application/fhir+json')
+                .send(resource)
+                .expect(200)
+                .then((response: any) => {
+                    resourceChecks(response, file)
+                })
+        });
+
+    })
+});
+
+describe('Parsing folder OperationDefinition', () => {
+    const dir = '../OperationDefinition';
+    const list = fs.readdirSync(dir);
+    list.forEach(function (file) {
+        file = dir + "/" + file;
+        const resource: any = fs.readFileSync(dir + "/" + file, 'utf8');
+
+        it('Validate ' + file, async () => {
+            await client()
+                .post('/$validate')
+                .set("Content-Type", getContentType(file))
+                .set("Accept", 'application/fhir+json')
+                .send(resource)
+                .expect(200)
+                .then((response: any) => {
+                    resourceChecks(response, file)
+                })
+        });
+
+    })
+});
+
+describe('Parsing folder StructureDefinition', () => {
+    const dir = '../StructureDefinition';
+    const list = fs.readdirSync(dir);
+    list.forEach(function (file) {
+        file = dir + "/" + file;
+        const resource: any = fs.readFileSync(dir + "/" + file, 'utf8');
+
+        it('Validate ' + file, async () => {
+            await client()
+                .post('/$validate')
+                .set("Content-Type", getContentType(file))
+                .set("Accept", 'application/fhir+json')
+                .send(resource)
+                .expect(200)
+                .then((response: any) => {
+                    resourceChecks(response, file)
+                })
+        });
+
+    })
+});
+
+
 describe('Testing validation api is functioning', () => {
     it('validation functionality test', async () => {
         await client()
